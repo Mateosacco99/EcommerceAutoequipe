545fe3f51a63815ee411f62b4a07ad471b7d47be
Mateosacco99 <mateosacco99@gmail.com>
2026-01-21 21:53:30 -0300

    Fix: Quitando itemCount de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar.jsx'
 import ItemListContainer from './components/ItemListContainer.jsx'
-import ItemCount from './components/ItemCount.jsx'
+import ItemCount from './examples/ItemCount.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <NavBar />
       <ItemListContainer mensaje="Bienvenidos a AutoEquipe!" />
-      <ItemCount />
     </>
   )
 }
