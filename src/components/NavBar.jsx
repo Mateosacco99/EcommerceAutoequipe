a30733d3d8e648a4ad3388e6606950c87f0e38b1
Mateosacco99 <mateosacco99@gmail.com>
2026-01-21 21:30:56 -0300

    Fix: Mas arreglos en estilos del NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,15 @@
-import '../styles/navBar.module.scss';
+import styles from '../styles/navBar.module.scss';
 import Cart from './Cart';
 import AutoEquipe from '../assets/AutoEquipe.png';
 
 const NavBar = () => {
     return (
-        <nav className="nav-container">
-            <h1><img className="logo" src={AutoEquipe} alt="AutoEquipe Logo" /></h1>
-            <a href="#" className="nav-item">Home</a>
-            <a href="#" className="nav-item">Products</a>
-            <a href="#" className="nav-item">About</a>
-            <a href="#" className="nav-item">Contact</a>
+        <nav className={styles['nav-container']}>
+            <h1><img className={styles.logo} src={AutoEquipe} alt="AutoEquipe Logo" /></h1>
+            <a href="#" className={styles['nav-item']}>Home</a>
+            <a href="#" className={styles['nav-item']}>Products</a>
+            <a href="#" className={styles['nav-item']}>About</a>
+            <a href="#" className={styles['nav-item']}>Contact</a>
             <Cart />
         </nav>
     );
