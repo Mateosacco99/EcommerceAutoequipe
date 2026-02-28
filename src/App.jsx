202f50195013e9fbe48e5cbbdc912698c8934047
Mateosacco99 <mateosacco99@gmail.com>
2026-02-28 13:30:55 -0300

    Feat: Quitando ejemplos de clase (rama main)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar.jsx'
 import ItemListContainer from './components/ItemListContainer.jsx'
-import { withLogging } from './hocs/withLogging.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ItemDetailContainer from './components/ItemDetailContainer.jsx'
 import CategoriaContainer from './components/CategoriaContainer.jsx'
 import About from './components/About.jsx'
 import Error from './components/Error.jsx'
-import FetchApi from './examples/FetchApi.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 
 function App() {
-  const ItemListContainerHOC = withLogging(ItemListContainer);
 
   return (
     <CartProvider>
