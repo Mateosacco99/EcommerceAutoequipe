41b8bab2f0670bb152b44c2d5506735b1262e8ce
Mateosacco99 <mateosacco99@gmail.com>
2026-03-04 23:29:04 -0300

    Fix: Arreglando imagen

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
     <div>
        <h1 className={styles.h1}>Error 404: Página no encontrada</h1>
        <p className={styles.p}>Lo sentimos, la página que estás buscando no existe.</p>
-       <img className={styles.imagenError} src='img/bib-meditate.jpg' alt="Error 404" />
+       <img className={styles.imagenError} src='/img/bib-meditate.jpg' alt="Error 404" />
     </div>
   )
 }
